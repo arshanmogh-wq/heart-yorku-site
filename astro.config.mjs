9a374effbba1b9bae35c078048854c2f63516dd6
AriyanAzami <ariyan.azami84@gmail.com>
2026-01-20 22:40:00 -0500

    Improved version to develop SEO and better looking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static'
+  site: 'https://heart-yorku.netlify.app', // Placeholder URL
+  output: 'static',
+  integrations: [sitemap()],
 });
